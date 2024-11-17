8be47590d0e374ffb8ff0b6cb41699285e73a1ae
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2024-11-17 12:46:23 -0300

    adicionado navbar para navegar entre os cadastros

---
+++ src/components/NavigationBar/index.js
@@ -0,0 +1,29 @@
+
+import {
+  Navbar,
+  NavbarBrand,
+  NavLink,
+  Nav,
+  NavItem
+} from 'reactstrap';
+
+function NavigationBar(args) {
+
+  return (
+    <div>
+      <Navbar {...args}>
+        <Nav className="mr-auto">
+          <NavbarBrand href="/">Services4YouNow</NavbarBrand>
+          <NavItem>
+            <NavLink href="/agents">Cadastrar Agentes</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/categories">Cadastrar Categorias</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavigationBar;
